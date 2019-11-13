3c7aa0de78d60d53ceee7d78877fafc1cabb5941
Frank S. Petrosky <fsp123@comcast.net>
2019-11-13 15:43:44 -0500

    park jdbc

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/JDBCParkDao.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/JDBCParkDao.java
@@ -1,13 +1,17 @@
 package com.techelevator.npgeek.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import com.techelevator.dao.model.Actor;
+import com.techelevator.model.Campground;
 import com.techelevator.npgeek.Park;
 import com.techelevator.npgeek.model.interfaces.ParkDao;
 
@@ -21,12 +25,49 @@ public class JDBCParkDao implements ParkDao {
 		this.jdbcTemplate = new JdbcTemplate(datasource);
 	}
 	
-	@Override
-	public List<Park> getAllParks() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+		
+		@Override
+	    public List<Park> getAllParks() {
+	        List<Park> parks = new ArrayList<>();
+	        String parkSearchSql = "SELECT parkcode, parkname, state, acreage, elevationinfeet, milesoftrail,"
+	        		+ "numberofcampsites, climate, yearfounded, annualvisitorcount, inspirationalquote,"
+	        		+ "inspirationalquotesource, parkdescription, entryfee, numberofanimalspecies FROM park";
+	        SqlRowSet results = jdbcTemplate.queryForRowSet(parkSearchSql);
+	        
+	        while(results.next() ) {
+				Park newPark = mapParkToRowSet(results);
+				parks.add(newPark);
+			}
+	        
+			return parks;
+	        
+	        
+	    }
+	    
+	 	    
+		private Park mapParkToRowSet(SqlRowSet results) {
+			Park newPark = new Park();
+			newPark.setParkCode(results.getString("parkcode"));
+			newPark.setParkName(results.getString("parkname"));
+			newPark.setState(results.getString("state"));
+			newPark.setAcreage(results.getInt("acreage"));
+			newPark.setElevationInFeet(results.getInt("elevationinfeet"));
+			newPark.setMilesOfTrail(results.getDouble("milesoftrail"));
+			newPark.setNumberOfCampsites(results.getInt("numberofcampsites"));
+			newPark.setClimate(results.getString("climate"));
+			newPark.setYearFounded(results.getInt("yearfounded"));
+			newPark.setAnnualVisitorCount(results.getInt("annualvisitorcount"));
+			newPark.setInspirationalQuote(results.getString("inspirationalquote"));
+			newPark.setInspirationalQuoteSource(results.getString("inspirationalquotesource"));
+			newPark.setParkDescription(results.getString("parkdescription"));
+			newPark.setEntryFee(results.getInt("entryfee"));
+			newPark.setNumberOfAnimalSpecies(results.getInt("numberofanimalspecies"));
+			
+			return newPark;
+		}
+		
+		
+	
 	@Override
 	public Park getParkByCode(String parkCode) {
 		// TODO Auto-generated method stub
