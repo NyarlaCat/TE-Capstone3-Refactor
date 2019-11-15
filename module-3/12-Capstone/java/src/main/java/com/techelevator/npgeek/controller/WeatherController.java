eef834c3b70bda150860016cbef780cac108b354
Julia R Brooks <jrbrookshorn@gmail.com>
2019-11-15 18:40:22 -0500

    finished temp conversion

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/controller/WeatherController.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/controller/WeatherController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techelevator.npgeek.Park;
@@ -24,7 +25,7 @@ public class WeatherController {
 	private ParkDao parkDao;
 	
 	@RequestMapping("/parkWeather")
-	public String displayWeatherDetails(@RequestParam String id, ModelMap modelMap, HttpSession session) {
+	public String displayWeatherDetails(@RequestParam String id, ModelMap modelMap) {
 		List<Weather> weather = weatherDao.getWeatherByParkCode(id);
 		Park weatherPark = parkDao.getParkByCode(id);
 		
@@ -32,5 +33,22 @@ public class WeatherController {
 		modelMap.put("weather", weather);
 		return "parkWeather";
 	} 
+	
+	@RequestMapping(path = "/parkWeather", method = RequestMethod.POST)
+	public String showTemeratureConversion( HttpSession session, @RequestParam String id) {
+		String temp = (String)session.getAttribute("Temperature");
+		if (temp == null) {
+			temp = "C";
+		} else if (temp.equals("C")) {
+			temp = "F";
+		} else if (temp.equals("F")) {
+			temp = "C";
+		}
+		
+		session.setAttribute("Temperature", temp);
+		
+		return"redirect:/parkWeather?id=" + id;
+	}
+
 
 }
