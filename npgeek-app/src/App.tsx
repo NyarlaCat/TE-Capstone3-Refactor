497c7eefbab0affbea15c67ef18751aa3285fe51
Narla <jrbrookshorn@gmail.com>
2026-09-11 16:12:51 -0400

    use header for all pages of app

--- npgeek-app/src/App.tsx
+++ npgeek-app/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
