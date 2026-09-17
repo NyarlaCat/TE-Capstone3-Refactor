bf18a3a6e6c311d6c0de1f549aefd426a79eba67
Narla <jrbrookshorn@gmail.com>
2026-09-17 17:53:14 -0400

    Type the response of /api/parkDetail in the ParkDetail component

--- npgeek-app/src/pages/ParkDetailPage.tsx
+++ npgeek-app/src/pages/ParkDetailPage.tsx
@@ -1,23 +1,31 @@
 import Typography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import type { Park } from '../types/park';
+import { Box } from '@mui/material';
 
 export default function ParkDetailPage() {
     const [searchParams] = useSearchParams();
-    const [parkDetail, setParkDetail] = useState(null);
+    const [parkDetail, setParkDetail] = useState<Park>();
 
 
     useEffect(() => {
         fetch(`/api/parkDetail?id=${searchParams.get('id')}`)
             .then((res) => {
                 if (!res.ok) throw new Error(`/api/parks responded with status ${res.status}`);
-                return res.json();
+                return res.json() as Promise<Park>;
             }).then(setParkDetail).catch((err) => console.error(err.message))
     }, []);
 
     return (
-        <Typography variant="h4" component="span">
-            Park Detail Page {parkDetail ? JSON.stringify(parkDetail) : 'Loading...'}
-        </Typography>
+        <Box>
+
+            <Typography variant="h4" component="span">
+                Park Detail Page
+            </Typography>
+            <Typography>
+                {parkDetail && parkDetail.parkName}
+            </Typography>
+        </Box>
     )
 }
