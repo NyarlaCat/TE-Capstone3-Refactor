3ee45a5a55a5fdbc8486ececdb096301649a4f5e
Narla <jrbrookshorn@gmail.com>
2026-09-23 17:42:13 -0400

    update park card CSS

--- npgeek-app/src/components/ParkCard.tsx
+++ npgeek-app/src/components/ParkCard.tsx
@@ -10,12 +10,12 @@ import { colors } from '../../designTokens/colors';
 
 export default function ParkCard({ park }: { park: Park }) {
     return (
-        <Card data-testid="park-card" sx={{ backgroundColor: colors.mediumGreen, display: 'flex', padding: '8px', flexDirection: { lg: 'row', xs: 'column' }, width: '100%' }}>
+        <Card data-testid="park-card" sx={{ backgroundColor: colors.mediumGreen, display: 'flex', padding: '8px', flexDirection: { lg: 'row', xs: 'column' }, width: '100%', alignItems: 'center' }}>
             <CardMedia>
                 <img src={`/parks/${park.imgCode}.jpg`} alt={`Photo of ${park.parkName}`} style={{ width: 'stretch' }} />
             </CardMedia>
             <Stack spacing={2} useFlexGap sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                <CardContent>
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
                     <Typography variant='h1' sx={{ fontSize: '2rem', color: colors.darkBrown, justifySelf: 'center' }} >{park.parkName}</Typography>
                     <Typography variant='body1' sx={{ fontSize: '1rem', padding: '32px 16px', background: colors.lightGreen, borderRadius: '8px', marginTop: '8px' }}>{park.parkDescription}</Typography>
                 </CardContent>
