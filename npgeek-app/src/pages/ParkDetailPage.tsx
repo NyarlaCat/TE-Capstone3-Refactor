d5b78a2882ed7216a4e1d2c028006063e037f398
Narla <jrbrookshorn@gmail.com>
2026-09-23 15:44:06 -0400

    format CSS for park facts

--- npgeek-app/src/pages/ParkDetailPage.tsx
+++ npgeek-app/src/pages/ParkDetailPage.tsx
@@ -2,13 +2,12 @@ import Typography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import type { Park } from '../types/park';
-import { Box, Card, CardContent, CardHeader, CardMedia } from '@mui/material';
+import { Box, Card, CardContent, CardHeader, CardMedia, Divider, Grid } from '@mui/material';
 import { colors } from '../../designTokens/colors';
 
 export default function ParkDetailPage() {
     const [searchParams] = useSearchParams();
     const [parkDetail, setParkDetail] = useState<Park>();
-
 
     useEffect(() => {
         fetch(`/api/parkDetail?id=${searchParams.get('id')}`)
@@ -30,50 +29,71 @@ export default function ParkDetailPage() {
                         <img src={`/parks/${parkDetail.imgCode}.jpg`} alt={`Photo of ${parkDetail.parkName}`} />
                     </CardMedia>
                 }
-                <CardContent>
-                    <Typography variant='subtitle1' sx={{ fontStyle: 'italic' }}>
-                        {parkDetail && parkDetail.inspirationalQuote}
+                <Typography variant='subtitle1' sx={{ fontStyle: 'italic' }}>
+                    {parkDetail && parkDetail.inspirationalQuote}
+                </Typography>
+                <Typography variant='subtitle2'>
+                    {parkDetail && `~ ${parkDetail.inspirationalQuoteSource}`}
+                </Typography>
+                <CardContent sx={{ padding: '32px 16px', background: colors.lightGreen, borderRadius: '8px', marginTop: '8px' }}>
+                    <Typography sx={{ fontSize: '1rem' }}>
+                        {parkDetail && parkDetail.parkDescription}
                     </Typography>
-                    <Typography variant='subtitle2'>
-                        {parkDetail && `~ ${parkDetail.inspirationalQuoteSource}`}
-                    </Typography>
-
+                    <Divider sx={{ width: '100%', margin: '16px 0px' }} />
+                    <Grid container spacing={2}>
+                        <Grid sx={{ fontSize: '1rem' }} >
+                            <Typography >
+                                <b>State: </b>{parkDetail && parkDetail.state}
+                            </Typography>
+                        </Grid>
+                        <Grid sx={{ fontSize: '1rem' }}>
+                            <Typography>
+                                <b>Acreage: </b>{parkDetail && parkDetail.acreage}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Elevation (ft): </b>{parkDetail && parkDetail.elevationInFeet}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Miles of Trail: </b>{parkDetail && parkDetail.milesOfTrail}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Number of Campsites: </b>{parkDetail && parkDetail.numberOfCampsites}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Climate: </b>{parkDetail && parkDetail.climate}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Year Founded: </b>{parkDetail && parkDetail.yearFounded}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Annual Visitor Count: </b>{parkDetail && parkDetail.annualVisitorCount}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Entry Fee: </b>${parkDetail && parkDetail.entryFee}
+                            </Typography>
+                        </Grid>
+                        <Grid>
+                            <Typography>
+                                <b>Number of Animal Species: </b>{parkDetail && parkDetail.numberOfAnimalSpecies}
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </Card>
-
-            <Typography>
-                State: {parkDetail && parkDetail.state}
-            </Typography>
-            <Typography>
-                Park Description: {parkDetail && parkDetail.parkDescription}
-            </Typography>
-            <Typography>
-                Acreage: {parkDetail && parkDetail.acreage}
-            </Typography>
-            <Typography>
-                Elevation (ft): {parkDetail && parkDetail.elevationInFeet}
-            </Typography>
-            <Typography>
-                Miles of Trail: {parkDetail && parkDetail.milesOfTrail}
-            </Typography>
-            <Typography>
-                Number of Campsites: {parkDetail && parkDetail.numberOfCampsites}
-            </Typography>
-            <Typography>
-                Climate: {parkDetail && parkDetail.climate}
-            </Typography>
-            <Typography>
-                Year Founded: {parkDetail && parkDetail.yearFounded}
-            </Typography>
-            <Typography>
-                Annual Visitor Count: {parkDetail && parkDetail.annualVisitorCount}
-            </Typography>
-            <Typography>
-                Entry Fee: {parkDetail && parkDetail.entryFee}
-            </Typography>
-            <Typography>
-                Number of Animal Species: {parkDetail && parkDetail.numberOfAnimalSpecies}
-            </Typography>
         </Box>
     )
 }
